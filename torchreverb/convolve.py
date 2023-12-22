f2b5fdb0bb6aa5ef027c61e65f4e881ebe43e9f4
francescopapaleo <francesco.papaleo@gmail.com>
2023-12-22 11:42:11 +0100

    refactor echo, convolve. added tests for both

--- torchreverb/convolve.py
+++ torchreverb/convolve.py
@@ -1,6 +1,7 @@
 import torch
 import torchaudio
-import torchaudio.functional as F
+from torchaudio.functional import convolve, fftconvolve
+import torch.nn.functional as F
 from typing import Union
 
 
@@ -13,28 +14,32 @@ class ConvolutionReverb:
     ) -> None:
         self.ir_file: str = ir_file
         self.conv_method: str = conv_method
-        self.mix: float = mix / 100
+        self.mix = mix
 
         self.ir_sig: Union[None, torch.Tensor] = None
         self.ir_sr: Union[None, int] = None
         self.load_ir()
 
     def load_ir(self) -> None:
-        ir_sig, ir_sr = torchaudio.load(self.ir_file)
-        self.ir_sig = ir_sig
-        self.ir_sr = ir_sr
-
+        self.ir_sig, _ = torchaudio.load(self.ir_file)
+        non_zero_indices = torch.nonzero(self.ir_sig.squeeze())
+        start_index = non_zero_indices[0]
+        end_index = non_zero_indices[-1] + 1
+        self.ir_sig = self.ir_sig[start_index:end_index]
+        
     def convolve(self, input_sig: torch.Tensor) -> torch.Tensor:
-        input_sig = input_sig / torch.linalg.vector_norm(input_sig, ord=2)
-        self.ir_sig = self.ir_sig / torch.linalg.vector_norm(self.ir_sig, ord=2)
+        length = input_sig.shape[1] + self.ir_sig.shape[1] - 1
+
         if self.conv_method == "fft":
-            wet_sig = F.fftconvolve(input_sig, self.ir_sig, mode="same")
+            wet_sig = fftconvolve(input_sig, self.ir_sig, mode="full")
         elif self.conv_method == "direct":
-            wet_sig = F.convolve(input_sig, self.ir_sig, mode="same")
+            wet_sig = convolve(input_sig, self.ir_sig, mode="full")
         else:
             raise ValueError("Invalid convolution method.")
+
+        input_sig = F.pad(input_sig, (0, length - input_sig.shape[1]))
         output_sig = wet_sig * self.mix + input_sig * (1 - self.mix)
-        print(output_sig.shape)
+        output_sig = output_sig / output_sig.abs().max()
         return output_sig
 
 
@@ -43,18 +48,14 @@ if __name__ == "__main__":
     output_file: str = "../audio/proc/output_convolve.wav"
     ir_file: str = "../audio/ir_analog/IR_AKG_BX25_1500ms_48kHz24b.wav"
     conv_method: str = "fft"
-    wet_mix: float = 1.0
+    wet_mix: float = 0.5
 
     # Load input signal
     input_sig, input_sr = torchaudio.load(input_file)
 
-    # Create ConvolutionReverb instance
     reverb = ConvolutionReverb(ir_file, conv_method, wet_mix)
-
-    # Convolve input signal
     output_sig = reverb.convolve(input_sig)
 
-    # Save the resulting audio with explicit dtype and format
     torchaudio.save(
         uri=output_file,
         src=output_sig,
@@ -69,6 +70,7 @@ if __name__ == "__main__":
     plt.figure()
     plt.subplot(3, 1, 1)
     time_axis = torch.arange(0, input_sig.shape[1]) / input_sr
+    time_axis = time_axis.unsqueeze(0)
     plt.plot(time_axis, input_sig.numpy(), label="input", alpha=0.5)
     plt.plot(time_axis, output_sig.numpy(), label="output", alpha=0.5)
     # plt.legend()
--- torchreverb/echo.py
+++ torchreverb/echo.py
@@ -1,54 +1,66 @@
 import torch
 import torchaudio
-import soundfile as sf
-import numpy as np
+import torch.nn.functional as F
 
 
 class Echo:
     def __init__(
         self,
         sr: int,
-        delay: float = 0.5,
-        mix: float = 1.0
+        delay: float,
+        mix: float,
     ) -> None:
         self.delay_samples: int = round(delay * sr)
-        self.mix: float = mix / 100
+        self.mix = mix
 
     def delay(self, input_sig: torch.Tensor) -> torch.Tensor:
-        delay_array = torch.zeros(self.delay_samples, dtype=input_sig.dtype)
-        wet_sig = torch.cat((delay_array, input_sig))
-        dry_sig = torch.cat((input_sig, delay_array))
+        # tensor.shape: [n_channels, time]
+        delay_array = torch.zeros([1, self.delay_samples])
+
+        dry_sig = torch.cat([input_sig, delay_array], dim=-1)
+        wet_sig = torch.cat([delay_array, input_sig], dim=-1)
+
         output_sig = wet_sig * self.mix + dry_sig * (1 - self.mix)
+        output_sig = output_sig / output_sig.abs().max()
         return output_sig
 
 
 if __name__ == "__main__":
-    input_file: str = "../audio/plk-fm-base.wav"
-    output_file: str = "../output_echo.wav"
-    wet_mix: float = 0.5
+    input_file: str = "../audio/raw/plk-fm-base.wav"
+    output_file: str = "../audio/proc/output_echo.wav"
+    delay: float = 0.25
+    mix: float = 0.5
 
-    # Load input signal
     input_sig, input_sr = torchaudio.load(input_file)
 
-    # Create Echo instance
-    echo = Echo(input_sr)
-
-    # Apply delay to the input signal
+    echo = Echo(sr=input_sr, delay=delay, mix=mix)
     output_sig = echo.delay(input_sig)
 
-    # Save the resulting audio with explicit dtype and format
-    sf.write(output_file, output_sig.numpy(), input_sr, format='WAV', subtype='PCM_24')
+    torchaudio.save(
+        uri=output_file,
+        src=output_sig,
+        sample_rate=input_sr,
+        channels_first=True,
+        format="wav",
+        encoding="PCM_S",
+        bits_per_sample=24)
+
+    # Pad the shorter signal to match the length of the longer one
+    max_length = max(input_sig.size(1), output_sig.size(1))
+    input_sig = F.pad(input_sig, (0, max_length - input_sig.size(1)))
+    output_sig = F.pad(output_sig, (0, max_length - output_sig.size(1)))
+
+    time = torch.linspace(0, max_length / input_sr, max_length)
 
     import matplotlib.pyplot as plt
     plt.style.use('seaborn-v0_8-whitegrid')
     plt.figure()
-    plt.subplot(3, 1, 1)
-    plt.plot(input_sig.numpy(), label="input", alpha=0.5)
-    plt.plot(output_sig.numpy(), label="output", alpha=0.5)
-    # plt.legend()
-    plt.subplot(3, 1, 2)
-    plt.specgram(input_sig.numpy(), Fs=input_sr, cmap='inferno')
-    plt.subplot(3, 1, 3)
-    plt.specgram(output_sig.numpy(), Fs=input_sr, cmap='inferno')
-    plt.tight_layout()
+    # Plot the input_sig and output_sig waveforms overlapped on the same plot
+    plt.plot(
+        time.numpy(), input_sig[0, :].numpy(),
+        label='Input signal', alpha=0.5)
+    plt.plot(
+        time.numpy(), output_sig[0, :].numpy(),
+        label='Output signal', alpha=0.5)
+    plt.legend()
     plt.show()
